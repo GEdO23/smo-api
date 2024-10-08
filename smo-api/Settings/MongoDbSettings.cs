0bb9be15fc20ad62abc4526a89e4f87feda0ad70
Gabriel Eringer <rm99632@fiap.com.br>
2024-10-08 09:56:43 -0300

    Configuring MongoDbSettings
    Signed-off-by: Gabriel Eringer <rm99632@fiap.com.br>

---
+++ smo-api/Settings/MongoDbSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace smo_api.Settings;
+
+public class MongoDbSettings
+{
+    public string? ConnectionString { get; set; }
+    public string? DatabaseName { get; set; }
+    public string? CollectionName { get; set; }
+}
