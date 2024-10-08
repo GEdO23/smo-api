6782b2882c01e8bca6e93b47766dce4b33087ae6
Gabriel Eringer <rm99632@fiap.com.br>
2024-10-08 08:07:38 -0300

    Adding Produto Model
    Signed-off-by: Gabriel Eringer <rm99632@fiap.com.br>

---
+++ smo-api/Models/Produto.cs
@@ -0,0 +1,19 @@
+﻿namespace smo_api.Models;
+
+public class Produto
+{
+    public long Id { get; set; }
+    public string Nome { get; set; }
+    public double Preco { get; set; }
+    public int Estoque { get; set; }
+    public string Descricao { get; set; }
+
+    public Produto(long id, string nome, double preco, int estoque, string descricao)
+    {
+        Id = id;
+        Nome = nome;
+        Preco = preco;
+        Estoque = estoque;
+        Descricao = descricao;
+    }
+}
