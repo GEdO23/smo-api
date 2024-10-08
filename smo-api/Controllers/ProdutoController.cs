af2d4752e43c0929c102b014ec268fdd9c750792
Gabriel Eringer <rm99632@fiap.com.br>
2024-10-08 09:58:15 -0300

    Adding ProdutoController with GetProdutos method
    Signed-off-by: Gabriel Eringer <rm99632@fiap.com.br>

---
+++ smo-api/Controllers/ProdutoController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using smo_api.Models;
+using smo_api.Repositories;
+using smo_api.Settings;
+
+namespace smo_api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProdutoController : ControllerBase
+{
+    private readonly ProdutoRepository _produtoRepository;
+
+    public ProdutoController(IOptions<MongoDbSettings> mongoDbSettings)
+    {
+        var settings = mongoDbSettings.Value;
+        _produtoRepository = new ProdutoRepository(
+            settings.ConnectionString!,
+            settings.DatabaseName!,
+            settings.CollectionName!
+        );
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<ProdutoModel>>> GetProdutos()
+    {
+        var produtos = await _produtoRepository.GetProdutosAsync();
+        return Ok(produtos);
+    }
+}
